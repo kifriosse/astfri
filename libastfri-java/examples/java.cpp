69a21752ab689c3de8d8570b6d41cec1370279f8
Adam Kitko <adamktiko@gmail.com>
2024-12-12 21:49:14 +0100

    Load source code from file

--- libastfri-java/examples/java.cpp
+++ libastfri-java/examples/java.cpp
@@ -1,7 +1,42 @@
 #include <cstring>
 #include <cstdio>
+#include <sstream>
 #include <tree_sitter/api.h>
 #include <tree_sitter/tree-sitter-java.h>
+#include <fstream>
+#include <iostream>
+
+// function for loading content of a file into string
+char* load(const char* path) {
+  std::ifstream file(path);
+  if(!file.is_open()) {
+    std::string error = "Could not open file: " + std::string(path);
+    char* errorMsg = new char[error.size() + 1];
+    strcpy(errorMsg, error.c_str());
+    return errorMsg;
+  }
+  
+  std::ostringstream buffer;
+  buffer << file.rdbuf();
+  std::string str = buffer.str();
+
+  char* content = new char[str.size() + 1];
+  strcpy(content, str.c_str());
+  return content;
+}
+
+std::string load_code(const std::string path) {
+  std::ifstream file(path);
+  if(!file.is_open()) {
+    std::string error = "Could not open file: " + path;
+    return error;
+  }
+
+  std::ostringstream buffer;
+  buffer << file.rdbuf();
+
+  return buffer.str();
+}
 
 int main() {
   // Create a parser.
@@ -11,7 +46,7 @@ int main() {
   ts_parser_set_language(parser, tree_sitter_java());
 
   // Build a syntax tree based on source code stored in a string.
-  const char *source_code = "public class Dummy {};";
+  const char *source_code = load(std::string("/tmp/resources/java_dummy.txt").c_str());
   TSTree *tree = ts_parser_parse_string(
     parser,
     NULL,
