3e622462f5bc0b28c71deee21abcf8d4011de54a
Michal Mrena <mrena.michal@gmail.com>
2025-03-05 13:11:13 +0100

    Visually separated declarations

--- libastfri/inc/StmtFactory.hpp
+++ libastfri/inc/StmtFactory.hpp
@@ -23,24 +23,29 @@ public:
         Type* type,
         Expr* initializer
     );
+
     ParamVarDefStmt* mk_param_var_def(
         std::string name,
         Type* type,
         Expr* initializer
     );
+
     MemberVarDefStmt* mk_member_var_def(
         std::string name,
         Type* type,
         Expr* initializer,
         AccessModifier access
     );
+
     GlobalVarDefStmt* mk_global_var_def(
         std::string name,
         Type* type,
         Expr* initializer
     );
+
     DefStmt* mk_def();
     DefStmt* mk_def(std::vector<VarDefStmt*> defs);
+
     FunctionDefStmt* mk_function_def();
     FunctionDefStmt* mk_function_def(
         std::string name,
@@ -48,6 +53,7 @@ public:
         Type* retType,
         CompoundStmt* body
     );
+
     MethodDefStmt* mk_method_def();
     MethodDefStmt* mk_method_def(
         ClassDefStmt* owner,
@@ -55,6 +61,7 @@ public:
         AccessModifier access,
         Virtuality virtuality
     );
+
     ConstructorDefStmt* mk_constructor_def();
     ConstructorDefStmt* mk_constructor_def(
         ClassDefStmt* owner,
@@ -63,34 +70,54 @@ public:
         CompoundStmt* body,
         AccessModifier access
     );
+
     BaseInitializerStmt* mak_base_initializer(
         std::string base,
         std::vector<Expr*> args
     );
+
     DestructorDefStmt* mk_destructor_def(
         ClassDefStmt* owner,
         CompoundStmt* body
     );
+
     GenericParam* mk_generic_param(std::string constraint, std::string name);
+
     InterfaceDefStmt* mk_interface_def();
     InterfaceDefStmt* mk_interface_def(std::string name);
+
     ClassDefStmt* mk_class_def();
     ClassDefStmt* mk_class_def(std::string name);
+
     CompoundStmt* mk_compound(std::vector<Stmt*> stmts);
+
     ReturnStmt* mk_return(Expr* val);
+
     ExprStmt* mk_expr(Expr* expr);
+
     IfStmt* mk_if(Expr* cond, Stmt* iftrue, Stmt* iffalse);
+
     CaseStmt* mk_case(Expr* expr, Stmt* body);
     CaseStmt* mk_case(std::vector<Expr*> exprs, Stmt* body);
+
     DefaultCaseStmt* mk_default_case(Stmt* body);
+
     SwitchStmt* mk_switch(Expr* expr, std::vector<CaseBaseStmt*> cases);
+
     WhileStmt* mk_while(Expr* cond, Stmt* body);
+
     DoWhileStmt* mk_do_while(Expr* cond, Stmt* body);
+
     ForStmt* mk_for(Stmt* init, Expr* cond, Stmt* step, Stmt* body);
+
     ThrowStmt* mk_throw(Expr* val);
+
     ContinueStmt* mk_continue();
+
     BreakStmt* mk_break();
+
     UnknownStmt* mk_uknown();
+
     TranslationUnit* mk_translation_unit();
     TranslationUnit* mk_translation_unit(
         std::vector<ClassDefStmt*> classes,
