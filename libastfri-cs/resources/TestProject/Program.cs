7424ca7dacd5696e9afe28edd7241f032e0b27af
Andrej Brandajsky <andrejbrandajsky@gmail.com>
2025-11-13 12:06:59 +0100

    Added C# test project into resources

---
+++ libastfri-cs/resources/TestProject/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+using System.Threading.Tasks;
+
+public class MainClass
+{
+    public string test = "test";
+    static void Main(string[] args)
+    {
+        decimal a_d = 1254.587m;
+        string b_s = @"test";
+
+        _ = 8;
+        string h = @"Multi
+        line
+        raw";
+
+
+        int a = 4, b = 5;
+        float c = (float)a;
+
+        ref readonly int r = ref a;
+
+        // 
+
+        int? nullable = null;
+        int bar = nullable ?? 5;
+
+        int foo = nullable is null ? bar : 6;
+
+        int a1;
+        const int a2 = 1;
+        a1 = a;
+    }
+}
+
+// public class Test
+// {
+//     struct A
+//     {
+//         public int a = 1;
+//         public A(int a)
+//         {
+//             this.a = a;
+//         }
+//     }
+//     private A a;
+
+//     public void foo(string bar)
+//     {
+
+//     }
+
+
+//     private void Bar(int b)
+//     {
+//         string? a = null;
+//         foo(a!);
+//     }
+// }
