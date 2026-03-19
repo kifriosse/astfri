1463ec0704acf191869752a530664b6353fc0d67
Michal Mrena <mrena.michal@gmail.com>
2026-03-19 14:53:46 +0100

    Concept adjustment

--- libastfri/impl/Concepts.hpp
+++ libastfri/impl/Concepts.hpp
@@ -4,41 +4,34 @@
 #include <concepts>
 #include <filesystem>
 
-#include "libastfri/inc/Stmt.hpp"
 #include "StmtFwd.hpp"
-
-namespace rapidjson {
-
-class Value;
-
-} // namespace rapidjson
 
 namespace astfri {
 
 /**
  * @brief TODO
  */
-template<typename C>
+template<typename Cfg, typename JsonNode>
 concept IsConfigClass = requires(
-    C cfg,
-    const rapidjson::Value& node,
+    Cfg cfg,
+    const JsonNode &node,
     const std::filesystem::path& path,
     int argc,
     char* argv[]
 ) {
-    { C::createFromJson(node) } -> std::same_as<C>;
-    { C::createFromJson(path) } -> std::same_as<C>;
-    { C::createDefault() } -> std::same_as<C>;
-    { C::createFromArgs(argc, argv) } -> std::same_as<C>;
+    { Cfg::createFromJson(node) } -> std::same_as<Cfg>;
+    { Cfg::createFromJson(path) } -> std::same_as<Cfg>;
+    { Cfg::createDefault() } -> std::same_as<Cfg>;
+    { Cfg::createFromArgs(argc, argv) } -> std::same_as<Cfg>;
 };
 
 /**
  * @brief TODO
  */
-template<typename Lib, typename Cfg>
+template<typename Lib, typename Cfg, typename JsonNode>
 concept IsInputLibInterface
     = requires(Cfg cfg, std::istream& ist, const std::filesystem::path& path) {
-          IsConfigClass<Cfg>;
+          IsConfigClass<Cfg, JsonNode>;
           { load_file(cfg, ist) } -> std::same_as<astfri::TranslationUnit>;
           { load_file(cfg, path) } -> std::same_as<astfri::TranslationUnit>;
           { load_project(cfg, path) } -> std::same_as<astfri::TranslationUnit>;
@@ -47,10 +40,10 @@ concept IsInputLibInterface
 /**
  * @brief TODO
  */
-template<typename Lib, typename Cfg>
+template<typename Lib, typename Cfg, typename JsonNode>
 concept IsOutputLibInterface
     = requires(Cfg cfg, std::ostream& ost, const astfri::TranslationUnit& root) {
-          IsConfigClass<Cfg>;
+          IsConfigClass<Cfg, JsonNode>;
           { process_ast(cfg, root) } -> std::same_as<void>;
           { process_ast(cfg, root, ost) } -> std::same_as<void>;
       };
