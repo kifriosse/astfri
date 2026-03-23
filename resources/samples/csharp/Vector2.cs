aff96cb42aa964faa3121e5f2b2551f9859bac77
Andrej Brandajsky <andrejbrandajsky@gmail.com>
2026-03-23 18:33:51 +0100

    Moved example files from example project in global resource to libasfri-cs resources and update Vector.cs file in global example project

--- resources/samples/csharp/Vector2.cs
+++ resources/samples/csharp/Vector2.cs
@@ -1,81 +1,177 @@
-global using System.Numerics;
-global using static System.Math;
-global using Cons = global::System.Console;
-using global::System.Security.Cryptography.X509Certificates;
-using Generics = System.Collections.Generic;
+// global using System.Numerics;
+// global using static System.Math;
+// global using Cons = global::System.Console;
+// using System.Drawing;
+// using System.Runtime.ExceptionServices;
+// using global::System.Security.Cryptography.X509Certificates;
+// using Generics = System.Collections.Generic;
 
-public interface MyInterface {
-    public void Method();
-}
-
-public class Vector2 : IComparable<Vector2>, IEquatable<Vector2>, MyInterface
+public class Vector : IComparable<Vector>, IEquatable<Vector> 
 {
+    float[] elements;
 
-    public static Vector2 Zero = new Vector2();
-    public float X { get; set; }
-    public float Y { get; set; }
+    public int Size => elements.Length;
 
-    public Vector2(float x = 0, float y = 0)
+    public Vector(int n)
     {
-        this.X = x;
-        this.Y = y;
+        elements = new float[n];
+    }
+    public Vector(params float[] elements) : this(elements.Length)
+    {
+        elements.CopyTo(this.elements);
     }
 
-    public void Method() {
-
+    public Vector(Vector other)
+    {
+        other.elements.CopyTo(elements);
     }
 
-    public Vector2() : this(0, 0)
+    public float this[uint index]
     {
-    }
-
-    public float this[int i]
-    {
-        get => i switch
-        {
-            0 => X,
-            1 => Y,
-            _ => throw new ArgumentException($"Invalid index : {i}")
-        };
+        get {
+            if (index >= elements.Length)
+                throw new IndexOutOfRangeException(
+                    $"Index: '{index}' out of range for vector of size: '{elements.Length}"
+                );
+            return elements[index];
+        }
         set
         {
-            switch (i)
-            {
-                case 0:
-                {
-                    X = value;
-                    break;
-                }
-                case 1:
-                    Y = value;
-                    break;
-                default: 
-                    throw new ArgumentException($"Invalid index : {i}");
-            }
+            if (index >= elements.Length)
+                throw new IndexOutOfRangeException(
+                    $"Index: '{index}' out of range for vector of size: '{elements.Length}"
+                );
+            elements[index] = value;
         }
     }
 
-    public static Vector2 operator +(Vector2 first, Vector2 second) =>
-        new(first.X + second.X, first.Y + second.Y);
+    public static Vector operator+ (Vector first, Vector second)
+    {
+        return Sum(first, second, false);
+    }
 
-    public static explicit operator int(Vector2 v) => (int)v.X;
+    public static Vector operator- (Vector first, Vector second)
+    {
+        return Sum(first, second, true);
+    }
+
+    public static Vector operator* (float scalar, Vector vec)
+    {
+        Vector result = new(vec.Size);
+        for (uint i = 0; i < result.Size; ++i)
+        {
+            result[i] = vec[i] * scalar;
+        }
+        return result;
+    }
+
+    public static Vector operator* (Vector vec, float scalar) => scalar * vec; 
+
+    public static Vector operator/ (Vector vec, float scalar)
+    {
+        if (scalar == 0) 
+            throw new DivideByZeroException("Cannot divide a vector by zero.");
+        return 1f / scalar * vec;
+    }
+
+    public float DotProduct(Vector other)
+    {
+        if (Size != other.Size)
+            throw new ArgumentException("Both vectors need to be have same size");
+        float result = 0;
+        for (uint i = 0; i < Size; ++i)
+        {
+            result += this[i] * other[i];
+        }
+        return result;
+    }
+
+    public int CompareTo(Vector? other)
+    {
+        return other == null 
+            ? 1
+            : elements.SequenceCompareTo(other.elements);
+    }
+
+    public bool Equals(Vector? other)
+    {
+        return other == null 
+            ? false
+            : elements.SequenceEqual(other.elements);
+    }
+
+    private static Vector Sum(Vector first, Vector second, bool subtract)
+    {
+        if (first.Size != second.Size)
+            throw new ArgumentException("Both vectors need to be have same size");
+        Vector result = new(first.Size);
+        for (uint i = 0; i < result.Size; ++i)
+        {
+            result[i] = first[i] + (subtract ? -second[i] : second[i]);
+        }
+        return result;
+    }
+}
+
+public class Vector2 : Vector
+{
+    public float X
+    {
+        get => this[0];
+        set => this[0] = value;
+    }
+    public float Y
+    {
+        get => this[1];
+        set => this[1] = value;
+    }
+
+    public Vector2(float x = 0, float y = 0) : base(x, y)
+    {
+    }
+
+    public Vector2(float x) : this(x, x)
+    {
+    }
 
     public void Deconstruct(out float x, out float y)
     {
         x = this.X;
         y = this.Y;
-        return;
+    }
+}
+
+public class Vector3 : Vector
+{
+    public float X
+    {
+        get => this[0];
+        set => this[0] = value;
+    }
+    public float Y
+    {
+        get => this[1];
+        set => this[1] = value;
     }
 
-    public int CompareTo(Vector2? other)
+    public float Z
     {
-        if (other == null) return 1;
-        return (X, Y).CompareTo((other.X, other.Y));
+        get => this[2];
+        set => this[2] = value;
     }
 
-    public bool Equals(Vector2? other)
+    public Vector3(float x = 0, float y = 0, float z = 0) : base(x, y, z)
     {
-        if (other == null) return false;
-        return X == other.X && Y == other.Y;
+    }
+
+    public Vector3(float val) : this(val, val, val)
+    {
+    }
+
+    public void Deconstruct(out float x, out float y, out float z)
+    {
+        x = this.X;
+        y = this.Y;
+        z = this.Z;
     }
 }
