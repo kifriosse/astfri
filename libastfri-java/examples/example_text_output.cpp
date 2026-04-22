d7a032bab0914c3306f6679774dd38058e10d79a
Adam Kitko <adamkitko@gmail.com>
2026-04-22 13:06:14 +0200

    Added Java sample source code for demonstration what Java library can process

--- libastfri-java/examples/example_text_output.cpp
+++ libastfri-java/examples/example_text_output.cpp
@@ -9,7 +9,7 @@ int main() {
     astfri::java::ASTBuilder* tb = new astfri::java::ASTBuilder();
 
     std::string sourceCode
-        = tb->load_project("/home/adamk/projects/astfri/libastfri-java/resources/aaus/structures");
+        = tb->load_file("/home/adamk/projects/astfri/resources/samples/java/Main.java");
     TSTree* tree                = tb->make_syntax_tree(sourceCode);
     astfri::TranslationUnit* tu = tb->get_translation_unit(tree, sourceCode);
 
