ad3156cec851b77ced6a511a2f8bed57917c0783
MarekPastorek <marekpastorek1@gmail.com>
2026-05-15 16:57:08 +0200

    abstract builder reworked

--- libastfri-text/impl/tools/AbstractBuilder.cpp
+++ libastfri-text/impl/tools/AbstractBuilder.cpp
@@ -1,30 +1,30 @@
-#include <libastfri-text/inc/tools/TextBuilder.hpp>
+#include <libastfri-text/inc/tools/AbstractBuilder.hpp>
 
 using namespace astfri::text;
 
-std::string& TextBuilder::get_builded_text()
+std::string& AbstractBuilder::get_builded_text()
 {
     return m_buildedText;
 }
 
-void TextBuilder::reset_text_builder()
+void AbstractBuilder::reset_text_builder()
 {
     m_buildedText.clear();
     m_indentationLevel = 0;
     m_isEmptyLine      = true;
 }
 
-void TextBuilder::increase_indentation()
+void AbstractBuilder::increase_indentation()
 {
     ++m_indentationLevel;
 }
 
-void TextBuilder::decrease_indentation()
+void AbstractBuilder::decrease_indentation()
 {
     --m_indentationLevel;
 }
 
-void TextBuilder::write_opening_curl_bracket(bool const& onNewLine, bool incIndent)
+void AbstractBuilder::write_opening_curl_bracket(bool const& onNewLine, bool incIndent)
 {
     if (onNewLine)
     {
@@ -42,7 +42,7 @@ void TextBuilder::write_opening_curl_bracket(bool const& onNewLine, bool incInde
     }
 }
 
-void TextBuilder::write_closing_curl_bracket(bool decIndent)
+void AbstractBuilder::write_closing_curl_bracket(bool decIndent)
 {
     write_new_line();
     if (decIndent)
@@ -52,7 +52,7 @@ void TextBuilder::write_closing_curl_bracket(bool decIndent)
     write_right_bracket("}");
 }
 
-void TextBuilder::write_comma_space()
+void AbstractBuilder::write_comma_space()
 {
     write_separator(",");
     write_space();
--- libastfri-text/inc/tools/AbstractBuilder.hpp
+++ libastfri-text/inc/tools/AbstractBuilder.hpp
@@ -1,18 +1,18 @@
-#ifndef LIBASTFRI_TEXT_TEXT_BUILDER
-#define LIBASTFRI_TEXT_TEXT_BUILDER
+#ifndef LIBASTFRI_TEXT_ABSTRACT_BUILDER
+#define LIBASTFRI_TEXT_ABSTRACT_BUILDER
 
 #include <string>
 
 namespace astfri::text
 {
-    class TextBuilder
+    class AbstractBuilder
     {
+    protected:
         std::string m_buildedText = "";
         int m_indentationLevel    = 0;
-    protected:
-        bool m_isEmptyLine = true;
+        bool m_isEmptyLine        = true;
     public:
-        virtual ~TextBuilder() = default;
+        virtual ~AbstractBuilder() = default;
         //
         std::string& get_builded_text();
         void reset_text_builder();
@@ -21,17 +21,15 @@ namespace astfri::text
         void write_opening_curl_bracket(bool const& onNewLine, bool incIndent = true);
         void write_closing_curl_bracket(bool decIndent = true);
         void write_comma_space();
-        //
-        virtual void write_text(std::string_view text)        = 0;
-        virtual void write_separator(std::string_view sep)    = 0;
+        // 3.1 GENERAL
+        virtual void write_text(std::string_view text) = 0;
+        virtual void write_new_line()                  = 0;
+        virtual void write_space()                     = 0;
+        // 3.2.1 OPERATORS
         virtual void write_left_bracket(std::string_view br)  = 0;
         virtual void write_right_bracket(std::string_view br) = 0;
-        //
-        virtual void write_unknown_type() = 0;
-        virtual void write_unknown_expr() = 0;
-        virtual void write_unknown_stmt() = 0;
-        virtual void write_new_line()     = 0;
-        virtual void write_space()        = 0;
+        // 3.2.2 SEPARATORS
+        virtual void write_separator(std::string_view sep) = 0;
     };
 }
 
