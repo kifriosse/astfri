3227f47e99cb8d615d81bc3835dd545e1b1bf62d
MarekPastorek <marekpastorek1@gmail.com>
2026-05-18 19:47:16 +0200

    implemented exporter

--- libastfri-text/inc/tools/Exporter.hpp
+++ libastfri-text/inc/tools/Exporter.hpp
@@ -7,14 +7,14 @@ namespace astfri::text
 {
     class Exporter
     {
-        TextLibConfig* m_config;
+        TextLibConfig* const m_config;
+        std::filesystem::path m_defPath;
     public:
         explicit Exporter(TextLibConfig* config);
         //
-        void export_pseudocode_file(std::string& text, std::ostream& ost);
-        void export_pseudocode_file(std::string& text);
-        void export_code_file(std::string& text, std::ostream& ost);
-        void export_code_file(std::string& text);
+        void export_file(std::string_view text, std::ostream* ost) const;
+    private:
+        std::string create_file_name() const;
     };
 }
 
